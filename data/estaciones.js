f36831c66bec33a5b4372205a639dfe43427e8ab
tomasnicolucci <tomas.nicolucci@gmail.com>
2023-05-12 20:22:33 -0300

    Agregado de mas funciones y mas estaciones en la base de datos

--- data/estaciones.js
+++ data/estaciones.js
@@ -3,17 +3,51 @@ const conn = require('./connection');
 const DATABASE = 'Test_estacion';
 const ESTACIONES = 'estaciones';
 const objectId = require('mongodb').ObjectId;
+const { ObjectId } = require('mongodb');
 
-async function getCoord(){
+async function  getEstaciones(){
     const connectiondb = await conn.getConnection();
-    const data = await connectiondb
+    const estaciones = await connectiondb
                         .db(DATABASE)
                         .collection(ESTACIONES)
                         .find()
                         .toArray();
-    let coord = data
+    return estaciones;
+}
+
+async function getEstacionById(id){
+    const connectiondb = await conn.getConnection();
+    const estacion = await connectiondb
+                        .db(DATABASE)
+                        .collection(ESTACIONES)
+                        .findOne({_id: new ObjectId(id)});
+    return estacion;
+}
+
+async function getCoord(){
+    const estaciones = await getEstaciones();
+    let coord = estaciones
                 .map((e)=>({coordenadas: e.coordenadas}));
     return coord;
 }
 
-module.exports = {getCoord}
+async function addEstacion(estacion){
+    const connectiondb = await conn.getConnection();
+    const estAgregada = await connectiondb
+                            .db(DATABASE)
+                            .collection(ESTACIONES)
+                            .insertOne(estacion);
+    return estAgregada;
+}
+
+async function deleteEstacion(id){
+    const connectiondb = await conn.getConnection();
+    const estacion = await getEstacionById(id)
+    const estEliminada = await connectiondb
+                            .db(DATABASE)
+                            .collection(ESTACIONES)
+                            .deleteOne(estacion);
+    return estEliminada;                        
+}
+
+module.exports = {getEstaciones, getEstacionById, getCoord, addEstacion, deleteEstacion}
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,25 @@ router.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
+router.get('/getEstaciones', async (req, res) => {
+  res.json(await data.getEstaciones())
+});
+
+router.get('/getEstacionById/:id', async (req, res) => {
+  res.json(await  data.getEstacionById(req.params.id))
+});
+
 router.get('/getCoord', async (req, res) => {
     res.json(await data.getCoord());
 });
-  
+
+router.post('/addEstacion', async (req, res) => {
+  const result = await data.addEstacion(req.body);
+  res.json(result);
+})
+
+router.delete('/deleteEstacion/:id', async (req, res) => {
+  res.json(await data.deleteEstacion(req.params.id));
+})
 
 module.exports = router;
